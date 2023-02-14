3322462241575d25da03bc444e356499ebc23af7
gula <cgula7@gmail.com>
2023-02-14 06:37:19 -0800

    Works

--- calendarScript.py
+++ calendarScript.py
@@ -13,7 +13,7 @@ from astral import LocationInfo
 from astral.sun import golden_hour
 from astral import SunDirection
 
-# (somewhat) WORKING thing to add new events to Google calendar. 
+# WORKING thing to add new events to Google calendar.  
 # 
 # Follow instruction here https://developers.google.com/calendar/api/quickstart/python to create a credentials.json file. This will not work without it.
 #
@@ -21,10 +21,15 @@ from astral import SunDirection
 #
 # Install Google api thing: pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
 #
-# ! If this causes errors, delete the token.json file
+# If this causes errors, delete the token.json file
 #
 # No considerations taken for DST
-#
+
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+YEAR = 2023
 
 
 city = LocationInfo("Seattle", "Washington", "America/Los_Angeles", 47.6062, -122.3321 )
@@ -37,7 +42,6 @@ def sunriseEnd(mnth, dy, yr):
     time1, time2 = golden_hour(city.observer, direction=SunDirection.RISING, tzinfo=("America/Los_Angeles"), date=datetime.date(yr, mnth, dy))
     return (time2.strftime("%Y-%m-%d") + "T" + time2.strftime("%H:%M:%S") + "-08:00")
 
-
 def sunsetStart(mnth, dy, yr):
     time1, time2 = golden_hour(city.observer, direction=SunDirection.SETTING, tzinfo=("America/Los_Angeles"), date=datetime.date(yr, mnth, dy))
     return (time1.strftime("%Y-%m-%d") + "T" + time1.strftime("%H:%M:%S") + "-08:00")
@@ -48,42 +52,66 @@ def sunsetEnd(mnth, dy, yr):
 
 
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/calendar']
-YEAR = 2023
+###
+### Sunrise
+###
+def sunrise(month, day):
 
-
-newSunrise = {
-  'summary': 'Morning Golden Hour',
-  'location': 'Seattle, WA',
-  'start': {
-    'dateTime': sunriseStart(2,24,YEAR),
-    'timeZone': 'America/Los_Angeles',
-  },
-  'end': {
-    'dateTime': sunriseEnd(2,24,YEAR),
-    'timeZone': 'America/Los_Angeles',
-  },
-  'recurrence': [
-    'RRULE:FREQ=DAILY;COUNT=1'
-  ],
-  'attendees': [
-    {'email': 'cgula7@gmail.com'}
-  ],
-  'reminders': {
-    'useDefault': False,
-    'overrides': [
-      {'method': 'email', 'minutes': 24 * 60},
-      {'method': 'popup', 'minutes': 10},
+  newSunrise = {
+    'summary': 'Morning Golden Hour',
+    'location': 'Seattle, WA',
+    'start': {
+      'dateTime': sunriseStart(month,day,YEAR),
+      'timeZone': 'America/Los_Angeles',
+    },
+    'end': {
+      'dateTime': sunriseEnd(month,day,YEAR),
+      'timeZone': 'America/Los_Angeles',
+    },
+    'recurrence': [
+      'RRULE:FREQ=DAILY;COUNT=1'
     ],
-  },
-}
+    'reminders': {
+      'useDefault': False,
+      'overrides': [
+        {'method': 'email', 'minutes': 24 * 60},
+        {'method': 'popup', 'minutes': 10},
+      ],
+    },
+  }
+  return newSunrise
+
+###
+### Sunset
+###
+def sunset(month, day):
+
+  newSunset = {
+    'summary': 'Evening Golden Hour',
+    'location': 'Seattle, WA',
+    'start': {
+      'dateTime': sunsetStart(month,day,YEAR),
+      'timeZone': 'America/Los_Angeles',
+    },
+    'end': {
+      'dateTime': sunsetEnd(month,day,YEAR),
+      'timeZone': 'America/Los_Angeles',
+    },
+    'recurrence': [
+      'RRULE:FREQ=DAILY;COUNT=1'
+    ],
+    'reminders': {
+      'useDefault': False,
+      'overrides': [
+        {'method': 'email', 'minutes': 24 * 60},
+        {'method': 'popup', 'minutes': 10},
+      ],
+    },
+  }
+  return newSunset
 
 
 def main():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
     # Stolen from Google https://developers.google.com/calendar/api/quickstart/python
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
@@ -106,14 +134,29 @@ def main():
     try:
         service = build('calendar', 'v3', credentials=creds)
 
+        j = 1
+        while j <= 12:
+          # API call
+          i = 1
+          while i <= 31:
+            try:
+              event = service.events().insert(calendarId='cgula7@gmail.com', body=sunrise(j,i)).execute()
+              print( 'Event created: %s' % (event.get('htmlLink')))
 
-        # API call
-        event = service.events().insert(calendarId='cgula7@gmail.com', body=newSunrise).execute()
-        print( 'Event created: %s' % (event.get('htmlLink')))
-        print(event)
+              event = service.events().insert(calendarId='cgula7@gmail.com', body=sunset(j,i)).execute()
+              print( 'Event created: %s' % (event.get('htmlLink')))
+              
+              
+            except:
+              print("Error! Day = ", i, ", Month = ", j)
+          i += 1
+        j += 1
 
     except HttpError as error:
         print('An error occurred: %s' % error)
+
+
+
 
 if __name__ == '__main__':
     main()
